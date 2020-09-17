db6c240d3f459ab1cdc4a8fb7558570d4bb91fe3
chen-tan <1154638108@qq.com>
2020-09-17 23:06:20 +0800

    tree

--- App.vue
+++ App.vue
@@ -1,28 +1,73 @@
 <template>
-    <div id="app">
-        <date-picker :date="date" @change-date="changeDate"/>
-    </div>
+    <base-tree :data="data" />
 </template>
 
 <script>
-import DatePicker from './DatePicker';
+import BaseTree from './Tree';
 export default {
     components:{
-        DatePicker,
+        BaseTree,
     },
     data(){
         return {
-            date:new Date(),
-        }
-    },
-    methods:{
-        changeDate(day){
-            this.date=day;
+            data: [
+                {
+                    label: "一级 1",
+                    children: [
+                    {
+                        label: "二级 1-1",
+                        children: [
+                        {
+                            label: "三级 1-1-1"
+                        }
+                        ]
+                    }
+                    ]
+                },
+                {
+                    label: "一级 2",
+                    children: [
+                    {
+                        label: "二级 2-1",
+                        children: [
+                        {
+                            label: "三级 2-1-1"
+                        }
+                        ]
+                    },
+                    {
+                        label: "二级 2-2",
+                        children: [
+                        {
+                            label: "三级 2-2-1"
+                        }
+                        ]
+                    }
+                    ]
+                },
+                {
+                    label: "一级 3",
+                    children: [
+                    {
+                        label: "二级 3-1",
+                        children: [
+                        {
+                            label: "三级 3-1-1"
+                        }
+                        ]
+                    },
+                    {
+                        label: "二级 3-2",
+                        children: [
+                        {
+                            label: "三级 3-2-1"
+                        }
+                        ]
+                    }
+                    ]
+                }
+            ]
         }
     }
 }
 </script>
-
-<style scoped>
-    
-</style>
